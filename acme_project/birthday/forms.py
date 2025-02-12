b3a78a1618c9907ae4da22a008ae5635b4ed931c
Alexander Pack <pack.alexander@yandex.ru>
2025-02-12 15:01:10 +0300

    add class ModelForm

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,7 +1,11 @@
 # birthday/forms.py
 from django import forms
 
+# Импортируем класс модели Birthday.
+from .models import Birthday
 
+"""
+# Пример использования класса форм:
 class BirthdayForm(forms.Form):
     first_name = forms.CharField(label='Имя', max_length=20)
     last_name = forms.CharField(
@@ -11,3 +15,20 @@ class BirthdayForm(forms.Form):
         label='Дата рождения',
         widget=forms.DateInput(attrs={'type': 'date'})
     )
+"""
+
+
+# Пример использования класса ModelForm:
+class BirthdayForm(forms.ModelForm):
+    # Удаляем все описания полей.
+
+    # Все настройки задаём в подклассе Meta.
+    class Meta:
+        # Указываем модель, на основе которой должна строиться форма.
+        model = Birthday
+        # Указываем, что надо отобразить все поля.
+        fields = '__all__'
+        # Указываем, что для поля с ДР используется виджет.
+        widgets = {
+            'birthday': forms.DateInput(attrs={'type': 'date'})
+        }
