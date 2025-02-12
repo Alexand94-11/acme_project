a4e9f911ec4159ca6ab34226cb24c2fa1a163a9b
Alexander Pack <pack.alexander@yandex.ru>
2025-02-12 18:12:57 +0300

    add birthday_list and saving data in base

--- acme_project/birthday/urls.py
+++ acme_project/birthday/urls.py
@@ -6,4 +6,6 @@ app_name = 'birthday'
 
 urlpatterns = [
     path('', views.birthday, name='create'),
+    # Маршрут для вывода списка ДР.
+    path('list/', views.birthday_list, name='list')
 ]
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 from .forms import BirthdayForm
 # Импортируем из utils.py функцию для подсчёта дней.
 from .utils import calculate_birthday_countdown
+# Импортируем модель дней рождения для работы ф-ции birthday_list.
+from .models import Birthday
 
 
 def birthday(request):
@@ -24,12 +26,14 @@ def birthday(request):
         form = BirthdayForm()
     """
     # Тот же код, но с использованием трюка:
-    form = BirthdayForm(request.GET or None)
+    form = BirthdayForm(request.POST or None)
     # Создаём словарь контекста сразу после инициализации формы.
     context = {'form': form}
     # Если форма валидна...
     if form.is_valid():
-        # ...вызовем функцию подсчёта дней:
+        # ...сохраняем данные из формы в БД:
+        form.save()
+        # ...вызываем функцию подсчёта дней:
         birthday_countdown = calculate_birthday_countdown(
             # ...и передаём в неё дату из словаря cleaned_data.
             form.cleaned_data['birthday']
@@ -38,3 +42,11 @@ def birthday(request):
         context.update({'birthday_countdown': birthday_countdown})
     # Указываем нужный шаблон и передаём в него словарь контекста.
     return render(request, 'birthday/birthday.html', context)
+
+
+def birthday_list(request):
+    # Получаем все объекты модели Birthday из БД.
+    birthdays = Birthday.objects.all()
+    # Передаём их в контекст шаблона.
+    context = {'birthdays': birthdays}
+    return render(request, 'birthday/birthday_list.html', context)
