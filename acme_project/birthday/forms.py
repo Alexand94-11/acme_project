2967eb3fa7bd08fe531e7cc3e8a08ae291ef6932
Alexander Pack <pack.alexander@yandex.ru>
2025-02-13 01:35:51 +0300

    add validators

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,8 +1,13 @@
 # birthday/forms.py
 from django import forms
+# Импортируем класс ошибки валидации.
+from django.core.exceptions import ValidationError
 
 # Импортируем класс модели Birthday.
 from .models import Birthday
+
+# Множество с именами участников Ливерпульской четвёрки.
+BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
 
 """
 # Пример использования класса форм:
@@ -34,3 +39,23 @@ class BirthdayForm(forms.ModelForm):
                 format="%Y-%m-%d", attrs={'type': 'date'}
             )
         }
+
+    def clean_first_name(self):
+        # Получаем значение имени из словаря очищенных данных.
+        first_name = self.cleaned_data['first_name']
+        # Разбиваем полученную строку по пробелам
+        # и возвращаем только первое имя.
+        return first_name.split()[0]
+
+    def clean(self):
+        # Вызываем родительский метод clean, чтобы сработала проверка
+        # и в форме BirthdayForm.
+        super().clean()
+        # Получаем имя и фамилию из очищенных полей формы.
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        # Проверяем вхождение сочетания имени и фамилии во множество имён.
+        if f'{first_name} {last_name}' in BEATLES:
+            raise ValidationError(
+                'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
+            )
--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,9 +1,22 @@
 from django.db import models
 
+# Импортируем функцию-валидатор.
+from .validators import real_age
+
 
 class Birthday(models.Model):
     first_name = models.CharField('Имя', max_length=20)
     last_name = models.CharField(
         'Фамилия', blank=True, help_text='Необязательное поле', max_length=20
     )
-    birthday = models.DateField('Дата рождения')
+    # Добавляем валидатор для проверки возраста.
+    birthday = models.DateField('Дата рождения', validators=(real_age,))
+
+    class Meta:
+        # Проверка на уникальность записи:
+        constraints = (
+            models.UniqueConstraint(
+                fields=('first_name', 'last_name', 'birthday'),
+                name='Unique person constraint',
+            ),
+        )
